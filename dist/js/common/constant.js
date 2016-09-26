7c4b97a6f0d17cbde19155d63ab9752d6d118acb
unknown <wangweiyi@iqiyi.pps>
2016-09-26 17:01:13 +0800

    提交

---
+++ dist/js/common/constant.js
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview 常量定义
+ * @author WangWeiyi
+ *
+ * [Change Log]
+ * 2016-09-23 WangWeiyi created
+ */
+define(function(require, exports, module) {
+  module.exports =  {
+    SCREEN_PAD_MAX: 767,
+    SCREEN_PHONE_MAX: 479,
+    NAV_TOP: 60,
+    NAV_TOP_SM: 24
+  };
+});
---
+++ dist/js/list.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview 列表页js
+ * @author WangWeiyi
+ *
+ * [Change Log]
+ * 2016-09-23 WangWeiyi created
+ */
+define(function(require, exports, module) {
+  //导航栏交互
+  require("./common/nav");
+  //列表交互
+  require("./common/mod-list");
+});
