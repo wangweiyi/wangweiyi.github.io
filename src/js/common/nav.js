8b63a23b1b2356dc0e69f7807b6ad20212035b62
unknown <wangweiyi@iqiyi.pps>
2016-09-28 10:54:31 +0800

    导航逻辑修正

--- src/js/common/nav.js
+++ src/js/common/nav.js
@@ -3,10 +3,11 @@ define(function(require, exports, module) {
   var $win = $(window);
   var $body = $('body');
   var $container = $('#container'); //页面主内容区容器
-  var $navSwitch = $('#navSwitch'); //导航栏开关按钮  
+  var $navSwitch = $('#navSwitch'); //导航栏开关按钮
+  var $mainNav = $('#mainNav'); //导航栏
   var ceilMaxWidth = $body.data('ceiling-max-width') || 0; //页面的宽度小于此值时启用吸顶效果
   var ceilScroll = $body.data('ceiling-scroll') || 0; //页面滚动的高度大于此值时启用吸顶效果, 若值为一个元素选择器，则使用此元素的高度
-  var $ceilScroll = $(ceilScroll); //页面滚动高度参考元素，当滚动值大于此元素高度时启用吸顶效果
+  var $ceilRef = $($body.data('ceiling-ref')); //页面滚动高度参考元素，当滚动值大于此元素高度时启用吸顶效果
   var constant = require("./constant");
 
   $navSwitch.click(function() {
@@ -16,7 +17,7 @@ define(function(require, exports, module) {
   //点击页面空白处关闭导航
   $(document).click(function(e) {
   	var $source = $(e.target);
-  	if ( !$source.is($navSwitch) && !$source.parents('nav').length ) { 
+  	if ( !$source.is($navSwitch) && !$source.is($mainNav) && !$source.parents('#mainNav').length ) { 
   		$body.removeClass('view-nav');
   	}
   });
@@ -28,7 +29,7 @@ define(function(require, exports, module) {
       return;
     }
 
-    var cs = $ceilScroll.outerHeight() || ceilScroll;
+    var cs = $ceilRef.outerHeight() || ceilScroll;
     if ( $win.scrollTop() > cs ) {
       $body.addClass('nav-ceiling');
     } else {
